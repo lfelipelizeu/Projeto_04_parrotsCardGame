4cf367dea0c36153cf2fbfe11bb8a7aabfa903fb
Luis Felipe Elizeu <l_felipelizeu@hotmail.com>
2021-07-27 22:24:37 -0300

    Implementada função de distribuir as cartas de acordo com a quantidade fornecida e embaralhadas

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,14 @@
+const baralho = Array.from(document.querySelectorAll(".carta"));
+
+function converterBaralho() {
+    let i;
+    for (i = 0; i < baralho.length; i++) {
+        baralho[i] = baralho[i].outerHTML;
+    }
+}
+
+converterBaralho();
+
 let qtd_cartas;
 
 while ( qtd_cartas === undefined ) {
@@ -5,9 +16,34 @@ while ( qtd_cartas === undefined ) {
 
     if ( qtd>=4 && qtd<=14 && qtd%2===0 ) {
          qtd_cartas = qtd;
-         alert(qtd_cartas);
+    }
+    else {
+        alert("Insira um número par entre 4 e 14!");
     }
 }
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function montarJogo() {
+    const cartas_exibidas = [];
+
+    let i;
+    for ( i = 0; i < qtd_cartas; i++ ) {
+        cartas_exibidas.push(baralho[i]);
+    }
+
+    cartas_exibidas.sort(comparador);
+
+    const jogo = document.querySelector(".conteudo");
+    jogo.innerHTML = "";
+    for ( i = 0; i < qtd_cartas; i++ ) {
+        jogo.innerHTML += cartas_exibidas[i];
+    }
+}
+
+montarJogo();
 
 function girarCarta(carta) {
     const verso = carta.querySelector(".verso");
